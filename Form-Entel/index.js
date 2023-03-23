b1fa7611b2d872700879056e4b0de6d5e7a6d9b0
Januha88 <januharyg@.com>
2023-03-23 16:12:20 -0300

    latest code update

--- Form-Entel/index.js
+++ Form-Entel/index.js
@@ -1,44 +1,26 @@
-
 // Obtener el formulario y el botón de guardar
-const form = documentElementById("form");
-// se crea la variable y se agrega un evento al botón de guardar
-const sendBtn = documentElementById("btnEnviar").addEvenListener("click", function(event){
+const form = document.getElementById("form");
+
+// Crear un objeto para almacenar los datos del formulario
+const formData = {
+    fullName: document.getElementById('fullName').value,
+    rut: document.getElementById('rut').value,
+    patente: document.getElementById('patente').value,
+    marca: document.getElementById('marca').value,
+    modelo: document.getElementById('modelo').value,
+    color: document.getElementById('color').value,
+};
+
+// Agregar un evento al botón de guardar
+const sendBtn = document.getElementById("btnEnviar").addEventListener("click", function(event){
     event.preventDefault(); //evita que se envíe el formulario
 
-    //crear un objeto para almacenar los valores del formulario
-
-    const formData = {
-
-        fullName: getElementById('fullName').value,
-        rut: getElementById('rut').value,
-        patente: getElementById('patente').value,
-        marca: getElementById('marca').value,
-        modelo: getElementById('modelo').value,
-        color: getElementById('color').value,
-    }
-
-    //guardar los datos en el localStorage
-
-    const saveFormData = JSON.parse(localStorage.getItem('saveFormData') || '[]');
+    // Guardar los datos en el localStorage
+   const saveFormData = JSON.parse(localStorage.getItem('saveFormData') || '[]');
     saveFormData.push(formData);
     localStorage.setItem('saveFormData', JSON.stringify(saveFormData));
 
-    //redirigir al usuario a la página de lista de formulario
-    window.location.href = './list.html'
-})
+    // Redirigir al usuario a la página de lista de formulario
+    window.location.href = './list.html';
+});
 
-//Agregar los nuevos datos al arreglo de datos almacenados
-
-recordsData.push(data);
-
-//guardar los datos actualizados en el local storage 
-localStorage.setItem("formData", JSON.stringify(recordsData));
-
-//mostrar los datos en la lista
-const listForm = document.getElementById
-
-
-
-
-
-   
--- Form-Entel/list.js
+++ Form-Entel/list.js
@@ -1,3 +1,30 @@
-//mostrar los datos guardados en la página de lista de formularios
-const saveFormData = JSON.parse(localStorage.getItem('saveFormData') || '[]');
-const formList= document.getElementById
+// Obtener los datos guardados en el localStorage
+const savedFormData = JSON.parse(localStorage.getItem('saveFormData') || '[]');
+
+// Crear un elemento para mostrar los datos y agregarlo al HTML
+const formList = document.getElementById('formList');
+
+savedFormData.forEach(function(formData){
+    const listItem = document.createElement('li');
+    const fullName = document.createElement('p');
+    fullName.textContent = formData.fullName;
+    const rut = document.createElement('p');
+    rut.textContent = formData.rut;
+    const patente = document.createElement('p');
+    patente.textContent = formData.patente;
+    const marca = document.createElement('p');
+    marca.textContent = formData.marca;
+    const modelo = document.createElement('p');
+    modelo.textContent = formData.modelo;
+    const color = document.createElement('p');
+    color.textContent = formData.color;
+
+    listItem.appendChild(fullName);
+    listItem.appendChild(rut);
+    listItem.appendChild(patente);
+    listItem.appendChild(marca);
+    listItem.appendChild(modelo);
+    listItem.appendChild(color);
+
+    formList.appendChild(listItem);
+});
